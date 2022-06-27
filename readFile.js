053c25c1e77a030a3bb744f9eb11b5f7fb0eda58
Gustavo Pospi <gutxander@gmail.com>
2022-06-27 19:51:59 -0300

    fix: remoção if no readFile

--- readFile.js
+++ readFile.js
@@ -3,8 +3,6 @@ const fs = require('fs/promises');
 const readFile = async () => {
   const file = await fs.readFile('./talker.json', 'utf-8');
 
-  if (file.length < 1) return null;
-
   return JSON.parse(file);
 };
 
